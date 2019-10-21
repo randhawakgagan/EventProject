d7315d0d70610177f96be1cb916cc3f409c47d15
randhawakgagan <randhawakgagan@gmail.com>
2019-10-20 20:16:07 -0400

    First Commit

---
+++ src/main/java/com/example/demo/EventResponse.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import java.time.LocalDate;
+
+import java.util.List;
+
+public class EventResponse {
+public	List<Event> Data;
+public	LocalDate StartDate;
+public	LocalDate EndDate;
+
+}
+
+
